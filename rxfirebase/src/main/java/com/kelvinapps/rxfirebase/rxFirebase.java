3f39f338cf26bc80bcb2867481a0591087b01123
Nick Moskalenko <nick.moskalenko@gmail.com>
2016-05-16 07:27:07 +0300

    auth methods added, error handling improved

--- rxfirebase/src/main/java/com/kelvinapps/rxfirebase/rxFirebase.java
+++ rxfirebase/src/main/java/com/kelvinapps/rxfirebase/rxFirebase.java
@@ -2,29 +2,103 @@ package com.kelvinapps.rxfirebase;
 
 import android.support.annotation.NonNull;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
-import com.kelvinapps.rxfirebase.exceptions.FirebaseAuthProviderDisabledException;
-import com.kelvinapps.rxfirebase.exceptions.FirebaseExpiredTokenException;
-import com.kelvinapps.rxfirebase.exceptions.FirebaseGeneralException;
-import com.kelvinapps.rxfirebase.exceptions.FirebaseInvalidTokenException;
-import com.kelvinapps.rxfirebase.exceptions.FirebaseNetworkErrorException;
-import com.kelvinapps.rxfirebase.exceptions.FirebasePermissionDeniedException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
+import rx.functions.Action0;
+import rx.subscriptions.Subscriptions;
 
 /**
  * Created by Nick Moskalenko on 15/05/2016.
  */
-public class rxFirebase<T> {
+public class rxFirebase {
 
-    public Observable<T> observeSingleValue(@NonNull final Query query, @NonNull final Class<T> clazz) {
+    public static Observable<AuthData> authAnonymously(@NonNull final Firebase firebase) {
+        return Observable.create(new Observable.OnSubscribe<AuthData>() {
+            @Override
+            public void call(final Subscriber<? super AuthData> subscriber) {
+                firebase.authAnonymously(new ObservableAuthResultHandler(subscriber));
+            }
+        });
+    }
+
+    public static Observable<AuthData> authWithPassword(@NonNull final Firebase firebase,
+                                                        @NonNull final String email,
+                                                        @NonNull final String password) {
+        return Observable.create(new Observable.OnSubscribe<AuthData>() {
+            @Override
+            public void call(final Subscriber<? super AuthData> subscriber) {
+                firebase.authWithPassword(email, password, new ObservableAuthResultHandler(subscriber));
+            }
+        });
+    }
+
+    public static Observable<AuthData> authWithOAuthToken(@NonNull final Firebase firebase,
+                                                          @NonNull final String provider,
+                                                          @NonNull final String token) {
+        return Observable.create(new Observable.OnSubscribe<AuthData>() {
+            @Override
+            public void call(final Subscriber<? super AuthData> subscriber) {
+                firebase.authWithOAuthToken(provider, token, new ObservableAuthResultHandler(subscriber));
+            }
+        });
+    }
+
+    public static Observable<AuthData> authWithOAuthToken(@NonNull final Firebase firebase,
+                                                          @NonNull final String provider,
+                                                          @NonNull final Map<String, String> options) {
+        return Observable.create(new Observable.OnSubscribe<AuthData>() {
+            @Override
+            public void call(final Subscriber<? super AuthData> subscriber) {
+                firebase.authWithOAuthToken(provider, options, new ObservableAuthResultHandler(subscriber));
+            }
+        });
+    }
+
+    public static Observable<AuthData> authWithOAuthToken(@NonNull final Firebase firebase,
+                                                          @NonNull final String token) {
+        return Observable.create(new Observable.OnSubscribe<AuthData>() {
+            @Override
+            public void call(final Subscriber<? super AuthData> subscriber) {
+                firebase.authWithCustomToken(token, new ObservableAuthResultHandler(subscriber));
+            }
+        });
+    }
+
+    public static Observable<AuthData> observeAuthState(@NonNull final Firebase firebase) {
+
+        return Observable.create(new Observable.OnSubscribe<AuthData>() {
+            @Override
+            public void call(final Subscriber<? super AuthData> subscriber) {
+                final Firebase.AuthStateListener listener = firebase.addAuthStateListener(new Firebase.AuthStateListener() {
+                    @Override
+                    public void onAuthStateChanged(AuthData authData) {
+                        subscriber.onNext(authData);
+                    }
+                });
+
+                subscriber.add(Subscriptions.create(new Action0() {
+                    @Override
+                    public void call() {
+                        firebase.removeAuthStateListener(listener);
+                    }
+                }));
+            }
+        });
+    }
+
+    public static <T> Observable<T> observeSingleValue(@NonNull final Query query, @NonNull final Class<T> clazz) {
 
         return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
@@ -33,6 +107,7 @@ public class rxFirebase<T> {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         subscriber.onNext(dataSnapshot.getValue(clazz));
+                        subscriber.onCompleted();
                     }
 
                     @Override
@@ -45,7 +120,7 @@ public class rxFirebase<T> {
         });
     }
 
-    public Observable<List<T>> observeValuesList(final Query query, final Class<T> clazz) {
+    public static <T> Observable<List<T>> observeValuesList(final Query query, final Class<T> clazz) {
         return Observable.create(new Observable.OnSubscribe<List<T>>() {
             @Override
             public void call(final Subscriber<? super List<T>> subscriber) {
@@ -57,6 +132,7 @@ public class rxFirebase<T> {
                             items.add(postSnapshot.getValue(clazz));
                         }
                         subscriber.onNext(items);
+                        subscriber.onCompleted();
                     }
 
                     @Override
@@ -69,25 +145,28 @@ public class rxFirebase<T> {
         });
     }
 
-    private void onError(@NonNull Subscriber subscriber, @NonNull FirebaseError firebaseError) {
-        switch (firebaseError.getCode()) {
-            case FirebaseError.INVALID_TOKEN:
-                subscriber.onError(new FirebaseInvalidTokenException(firebaseError.getMessage()));
-                break;
-            case FirebaseError.AUTHENTICATION_PROVIDER_DISABLED:
-                subscriber.onError(new FirebaseAuthProviderDisabledException(firebaseError.getMessage()));
-                break;
-            case FirebaseError.EXPIRED_TOKEN:
-                subscriber.onError(new FirebaseExpiredTokenException(firebaseError.getMessage()));
-                break;
-            case FirebaseError.NETWORK_ERROR:
-                subscriber.onError(new FirebaseNetworkErrorException(firebaseError.getMessage()));
-                break;
-            case FirebaseError.PERMISSION_DENIED:
-                subscriber.onError(new FirebasePermissionDeniedException(firebaseError.getMessage()));
-                break;
-            default:
-                subscriber.onError(new FirebaseGeneralException(firebaseError.getMessage()));
+    private static void onError(@NonNull Subscriber subscriber, @NonNull FirebaseError firebaseError) {
+        subscriber.onError(new rxFirebaseException(firebaseError));
+    }
+
+
+    private static class ObservableAuthResultHandler implements Firebase.AuthResultHandler {
+
+        private final Observer<? super AuthData> observer;
+
+        private ObservableAuthResultHandler(Observer<? super AuthData> observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void onAuthenticated(AuthData authData) {
+            observer.onNext(authData);
+            observer.onCompleted();
+        }
+
+        @Override
+        public void onAuthenticationError(FirebaseError firebaseError) {
+            observer.onError(new rxFirebaseException(firebaseError));
         }
     }
 }
